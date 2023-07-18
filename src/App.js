8ed44c7eff9910cc3daf29c010a3455210612d30
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2023-07-18 21:01:00 +0300

    link eklendi
    link eklendi

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/aboutUs' element={<AboutUs />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/aboutUs">About </Link>
+        </nav>
+    )
+}
+
+export default Navbar
